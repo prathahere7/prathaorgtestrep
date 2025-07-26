3151d19b2ff4c8fad1ae4d3cc603c9ffd3cef28f
balbirkaria <balbirkaria.127188@marwadiuniversity.ac.in>
2025-07-26 09:29:27 +0530

    Update sample_script.sh

--- sample_script.sh
+++ sample_script.sh
@@ -6,6 +6,14 @@ ls -la
 echo "Showing current user:"
 whoami
 
+whoami
+
+# A vulnerable command using user input unsafely
+echo "Enter filename to view:"
+# A vulnerable command using user input unsafely
+echo "Enter filename to view:"
+sdada sasdasdladasd
+sdada sasdasdladasd
 # A vulnerable command using user input unsafely
 echo "Enter filename to view:"
 read filename
